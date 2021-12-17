f5fab20876fa94a15a33234adfb6f2a64342c836
fredouuce <fredoox974@hotmail.fr>
2021-12-17 17:20:58 +0400

    vue apollo premier commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createApolloProvider } from "@vue/apollo-option";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 
-createApp(App).use(router).mount("#app");
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  cache,
+  uri: "https://graphql.anilist.co/",
+});
+
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+});
+
+createApp(App).use(router).use(apolloProvider).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
+import Home from "../views/Home.vue";
+import Detail from "../views/Detail.vue";
 const routes = [
   {
     path: "/",
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/detail/:id",
+    name: "Detail",
+    component: Detail,
   },
 ];
 
